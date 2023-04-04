6bbde5a8a0ff09fce6f3e54853af252f0bdddb1f
Maryam Ali <128506461+Maaryam-Ali@users.noreply.github.com>
2023-04-04 04:46:40 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+// Question NO: 01 //
+
+//alert("Hey javasript");//
+
+// Question NO: 02 //
+
+// alert("JavaScript Alert \n\nError! Please enter a valid password.");//
+
+// Question NO: 03 //
+
+// alert("Javasript Alert \n\nWelcome to JS Land... \nHappy Coding!");//
+
+// Question NO: 04 //
+
+// alert("JavaSript Alert \n\nWelcome to JS Land... "); //
+// alert("JavaSript Alert \n\nHappy Coding! \nPrevent this page from creating additional dialogs."); //
+
+// Question No: 05 //
+
+alert("JavaScript Alert \n\nHello... I can run JS through my web browser's console"); 
